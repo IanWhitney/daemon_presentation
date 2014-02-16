98d9ca87e6136288810bfb1918a4bda2742b95dd
Ian Whitney <whit0694@umn.edu>
2014-02-16 13:43:37 -0600

    Simple Rails app for demonstration.
    Adding a simple Rails app which is part of the presentation. It will be
    used to create new users, which will be 'onboarded' by the daemon.

---
+++ customer_signup/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CustomerSignup::Application.config.secret_key_base = 'fb42e4fe425e87e0792b92de253bfddbd048077eb2319f9c90d57e7bc844ac5c38afb4d078802142a7ac0ff67eea71c5fef772299e64639118dfac61b0b37154'
---
+++ customer_signup/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CustomerSignup::Application.config.session_store :cookie_store, key: '_customer_signup_session'
